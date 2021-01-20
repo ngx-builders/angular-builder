c58ceff9a5b3e965cc164da198f80acadbc02f3f
Sunny <sunnyshh45@gmail.com>
2021-01-19 19:34:51 -0500

    ng update

--- src/app/references/references.component.spec.ts
+++ src/app/references/references.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ReferencesComponent } from './references.component';
 
@@ -6,7 +6,7 @@ describe('ReferencesComponent', () => {
   let component: ReferencesComponent;
   let fixture: ComponentFixture<ReferencesComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ReferencesComponent ]
     })
