5d5a732902aa78304f0fd7c3ac7e14c31bdbc075
Sunny <sunnyshh45@gmail.com>
2021-01-21 00:46:25 -0500

    removing unnecessary space

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -9,8 +9,6 @@ import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
-
-
 @NgModule({
   declarations: [],
   imports: [
--- src/app/references/references.module.ts
+++ src/app/references/references.module.ts
@@ -4,8 +4,6 @@ import { MaterialModule } from 'src/app/material/material.module';
 import { ReferencesRoutingModule } from './references-routing.module';
 import { ReferencesComponent } from './references.component';
 
-
-
 @NgModule({
   declarations: [ReferencesComponent],
   imports: [
